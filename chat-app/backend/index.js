9b80f19ca29d176ace702a9e524a8e3236ac530e
JanefrancessC <janefrancesschioma@gmail.com>
2026-05-26 12:49:10 +0100

    fix: Remove dotenv since not used

--- chat-app/backend/index.js
+++ chat-app/backend/index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(dotenv())
 
 const port = process.env.PORT || 3000;
 let nextMessageId = 1;
